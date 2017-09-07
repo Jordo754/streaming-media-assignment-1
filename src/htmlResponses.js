8e4b62d3c5578a76ee200a24d06c644fdf3945fd
Jordan Mato <jordo754@gmail.com>
2017-09-07 14:08:21 -0400

    corrected name for page3 variable

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
-const page2 = fs.readFileSync(`${__dirname}/../client/client3.html`);
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
